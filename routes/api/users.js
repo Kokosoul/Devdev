cb4ecd9f2ac58eb5b59a32d305ae3484e5ca2bd3
Yoshi <ghowell1228@gmail.com>
2019-09-11 23:02:40 -0400

    education

--- routes/api/users.js
+++ routes/api/users.js
@@ -3,7 +3,11 @@ const router = express.Router();
 const { check, validationResult } = require('express-validator')
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken')
 const User = require('../../models/User')
+const config = require('config')
+
+
 
 // @route   POST api/users
 // @desc    Register user
@@ -24,7 +28,7 @@ router.post('/', [
         // See if user exists
         let user = await User.findOne({ email })
         if (user) {
-            res.status(400).json({ errors: [{ msg: 'User already exists' }] });
+            return res.status(400).json({ errors: [{ msg: 'User already exists' }] });
         }
         // Get users gravatar
         const avatar = gravatar.url(email, {
@@ -44,7 +48,20 @@ router.post('/', [
         user.password = await bcrypt.hash(password, salt);
         await user.save();
         // Return jsonwebtoken
-        res.send('User registered');
+        const payload = {
+            user: {
+                id: user.id
+            }
+        }
+
+        jwt.sign(payload, config.get('jwtSecret'),
+            { expiresIn: 360000 },
+            (err, token) => {
+                if (err) throw err;
+                res.json({ token })
+            }
+        )
+
     } catch (err) {
         console.error(err.message);
         res.status(500).send('server error')
