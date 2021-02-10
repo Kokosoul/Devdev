de8a8685935c5406a4797e790daa84fddff43244
Jackie Xu <mac@jackies-mbp.home>
2021-02-10 18:34:17 -0500

    added test text

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,7 @@ import Landing from "./components/layout/Landing";
 import Login from "./components/auth/Login";
 import Register from "./components/auth/Register";
 import Alert from "./components/layout/Alert";
-
+// testing code
 // Redux
 import { Provider } from "react-redux";
 import store from "./store";
